4c0d5f6a815736d41c744ba517b99f17d54a7332
su530201 <olivier.kaufmann@umons.ac.be>
2021-10-17 10:10:50 +0200

    Fixes tests and changes package to lowercase

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,19 +1,19 @@
 import unittest
-from .context import GSDMA_2021
+from .context import gsdma_2021 as gsdma
 
 
 class CoreTestCase(unittest.TestCase):
     def test_fake_function(self):
-        actual = GSDMA_2021.core.fake_function()
+        actual = gsdma.core.fake_function()
         expected = "Hello world!"
         self.assertEqual(expected, actual)
 
     def test(self):
-        fake = GSDMA_2021.core.FakeClass();
+        fake = gsdma.core.FakeClass();
         actual = fake.name
         expected = 'unknown'
         self.assertEqual(expected, actual)
-        fake = GSDMA_2021.core.FakeClass(name="test")
+        fake = gsdma.core.FakeClass(name="test")
         actual = fake.name
         expected = 'test'
         self.assertEqual(expected, actual)
